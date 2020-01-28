43504dcf2e3c794d107a009efd09b5b2a07625e8
Zachary Williams <zwilliams.dev@gmail.com>
2020-01-28 12:04:12 -0500

    Checks if player has dirty money added notifications

--- server/main.lua
+++ server/main.lua
@@ -7,9 +7,14 @@ RegisterServerEvent('esx_moneywash:cleanMoney')
 AddEventHandler('esx_moneywash:cleanMoney', function(target)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
-    local cash = xPlayer.getMoney()
     local dirtyMoney = xPlayer.getAccount("black_money")
-    local cleanMoney = math.ceil(dirtyMoney.money - (dirtyMoney.money * Config.laundryTax))
-    xPlayer.addMoney(cleanMoney)
-    xPlayer.removeAccountMoney("black_money", dirtyMoney.money)
+    if dirtyMoney.money > 0 then
+        local cash = xPlayer.getMoney()
+        local cleanMoney = math.ceil(dirtyMoney.money - (dirtyMoney.money * Config.laundryTax))
+        xPlayer.addMoney(cleanMoney)
+        xPlayer.removeAccountMoney("black_money", dirtyMoney.money)
+        TriggerClientEvent('esx:showNotification', _source, '~g~Exchanged ~r~$'..dirtyMoney.money..' dirty money ~g~for $'..cleanMoney..' cash.')
+    else
+        TriggerClientEvent('esx:showNotification', _source, '~r~Come back when you have something to wash.')
+    end
 end)
